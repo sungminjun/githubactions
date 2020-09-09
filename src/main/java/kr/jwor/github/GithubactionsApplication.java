19433e6ff7664c14ebcc76afaf462cfd11d89177
Sungmin JUN <Sungmin JUN@host.docker.internal>
2020-09-10 07:29:24 +0900

    test commit: why gitignore failed?

---
+++ src/main/java/kr/jwor/github/GithubactionsApplication.java
@@ -0,0 +1,13 @@
+package kr.jwor.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/jwor/github/GithubactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.jwor.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
